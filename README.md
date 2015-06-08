825f2f8372c30783bc4e3d38d919e604f47be421
Ramanujam Srivatsan <sramanujam@pivotal.io>
2015-06-07 20:34:22 -0700

    adding flask-assets to manage js & css. plugged-in placeholder for pandas to query db

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
-Pushing the App to PCF
+Pre-requisites
+==============
+The conda_requirements.txt file lists all the python modules that are pre-requisites for this app.
+
+Code Organization
+==================
+predim/
+   server.py #Main module containing all controller code
+   static/
+      css/ #All user specified css. These will be bundled & minified into "gen/user_css.css" by Flask-Assets.    
+      data/   
+      img/    
+      js/ #All user specified javascript. These will all be bundled & minified into "gen/user_js.js" by Flask-Assets.    
+      vendor/ #All bootstrap.js related files (css & javascript)
+   templates/ #HTML templates
+      layout.html  #Base layout from which every page will inherit. This also contains javascript & css inserts
+      home.html #home page template
+      about.html #about page template
+      contact.html #contact page template
+
+Starting the app locally
+========================
+Run the following from the root directory:
+```
+python setup.py build;python setup.py install;python -m predim
+```
+This will bring up the app on http://localhost:9090
+
+Pushing the app to PCF
 ======================
 Run the following
 ```
--- predim/server.py
+++ predim/server.py
@@ -5,13 +5,35 @@
 
 import os
 from flask import Flask, render_template
+from flask.ext.assets import Bundle, Environment
 import pandas.io.sql as psql
 import psycopg2
 import logging
+import ConfigParser
 
+#init app
 app = Flask(__name__)
+
+#init logger
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
+
+#init flask assets
+bundles = {
+    'user_js': Bundle(
+           'js/heatmap.js',
+           output='gen/user.js',
+        ),
+    'user_css': Bundle(
+           'css/custom.css',
+           output='gen/user.css'
+        )   
+}
+assets = Environment(app)
+assets.register(bundles)
+
+#Initialize config parser
+conf = ConfigParser.ConfigParser()
 
 def index():
     """
@@ -60,5 +82,27 @@ def main():
         port = int(os.getenv("VCAP_APP_PORT"))
     else:
         #default port
-        port = 9090
-    app.run(host='0.0.0.0', debug=True, port=port)
+        default_port = 9090
+        #Read database credentials from user supplied file
+        basepath = os.path.dirname(__file__)
+        conf.read(os.path.join(basepath,'user.cred'))
+        logger.debug('Config sections:'+','.join(conf.sections()))
+        #host, port, user, database, password
+        host = conf.get('database_creds','host')
+        port = conf.get('database_creds','port')
+        user = conf.get('database_creds','user')
+        database = conf.get('database_creds','database')
+        password = conf.get('database_creds','password')
+        conn = psycopg2.connect("""dbname='{database}' user='{user}' host='{host}' port='{port}' password='{password}'""".format
+                     (
+                         database=database,
+                         host=host,
+                         port=port,
+                         user=user,
+                         password=password
+                     )
+               )
+        df = psql.read_sql('select x, x+random() as y from generate_series(1,10) x;',conn)
+        logger.debug('Read SQL: {nrows}'.format(nrows=df.size))
+
+    app.run(host='0.0.0.0', debug=True, port=default_port)
