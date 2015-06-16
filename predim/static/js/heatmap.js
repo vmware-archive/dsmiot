c5b4fca7727aa664aa5529841636685b40f9179b
Ramanujam Srivatsan <sramanujam@pivotal.io>
2015-06-15 18:06:59 -0700

    js & css fixes for proper rendering of heat map

--- predim/static/js/heatmap.js
+++ predim/static/js/heatmap.js
@@ -1,21 +1,25 @@
 function drillHeatmap(data){
-    var margin = { top: 50, right: 0, bottom: 100, left: 30 },
-        width = 960 - margin.left - margin.right;
+    var margin = { top: 100, right: 0, bottom: 100, left: 100 },
+        width = 1000 - margin.left - margin.right;
     var well_ids = d3.set(data.map(function(d){ 
                          return d.well_id;
                      }
                    )).values();
     var times = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"];    
-    var gridSize = 25,
+    var gridSize = 35,
         height = well_ids.length*gridSize,
-        legendElementWidth = gridSize*2,
         buckets = 9,
-        colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"]; // alternatively colorbrewer.YlGnBu[9]    
+        colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"], // alternatively colorbrewer.YlGnBu[9]    
+        legendElementWidth = gridSize*times.length/buckets;
 
     var colorScale = d3.scale.quantile()
-      .domain([0, buckets - 1, d3.max(data, function (d) { return d.prob; })])
+      .domain([0, d3.max(data, function (d) { return d.prob; })])
       .range(colors);
 
+    /* Clear existing elements */
+    d3.select("#hmap_spinner").html("")
+
+    /* Start painting heatmap */
     var svg = d3.select("#drillheatmap").append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
@@ -25,12 +29,12 @@ function drillHeatmap(data){
     var dayLabels = svg.selectAll(".dayLabel")
       .data(well_ids)
       .enter().append("text")
-        .text(function (d) { return d; })
+        .text(function (d) { return "well_"+d; })
         .attr("x", 0)
         .attr("y", function (d, i) { return i * gridSize; })
         .style("text-anchor", "end")
         .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
-        .attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); });
+        .attr("class", function (d, i) { return "dayLabel mono axis"; });
 
     var timeLabels = svg.selectAll(".timeLabel")
       .data(times)
@@ -40,13 +44,13 @@ function drillHeatmap(data){
         .attr("y", 0)
         .style("text-anchor", "middle")
         .attr("transform", "translate(" + gridSize / 2 + ", -6)")
-        .attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis"); });
+        .attr("class", function(d, i) { return ((i >= 8 && i <= 17) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis"); });
 
     var heatMap = svg.selectAll(".hour")
       .data(data)
       .enter().append("rect")
-      .attr("x", function(d) { return (d.hour - 1) * gridSize; })
-      .attr("y", function(d) { return (d.well_id - 1) * gridSize; })
+      .attr("x", function(d) { return (d.hour) * gridSize; })
+      .attr("y", function(d) { return (d.well_id) * gridSize; })
       .attr("rx", 4)
       .attr("ry", 4)
       .attr("class", "hour bordered")
@@ -66,103 +70,16 @@ function drillHeatmap(data){
 
     legend.append("rect")
     .attr("x", function(d, i) { return legendElementWidth * i; })
-    .attr("y", height)
+    .attr("y", -1.5*gridSize)
     .attr("width", legendElementWidth)
     .attr("height", gridSize / 2)
     .style("fill", function(d, i) { return colors[i]; });
 
     legend.append("text")
     .attr("class", "mono")
-    .text(function(d) { return "≥ " + Math.round(d); })
+    .text(function(d) { return "≥ " + Number(d).toFixed(2); })
     .attr("x", function(d, i) { return legendElementWidth * i; })
-    .attr("y", 0);
+    .attr("y", -2.0*gridSize);
 }
 
 
-function drillHeatmapOld(){
-    var margin = { top: 50, right: 0, bottom: 100, left: 30 },
-              width = 960 - margin.left - margin.right,
-              height = 430 - margin.top - margin.bottom,
-              gridSize = Math.floor(width / 24),
-              legendElementWidth = gridSize*2,
-              buckets = 9,
-              colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"], // alternatively colorbrewer.YlGnBu[9]
-              days = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
-              times = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"];
-
-
-      d3.tsv("data/data.tsv",
-        function(d) {
-          return {
-            day: +d.day,
-            hour: +d.hour,
-            value: +d.value
-          };
-        },
-        function(error, data) {
-          var colorScale = d3.scale.quantile()
-              .domain([0, buckets - 1, d3.max(data, function (d) { return d.value; })])
-              .range(colors);
-
-          var svg = d3.select("#drillheatmap").append("svg")
-              .attr("width", width + margin.left + margin.right)
-              .attr("height", height + margin.top + margin.bottom)
-              .append("g")
-              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-          var dayLabels = svg.selectAll(".dayLabel")
-              .data(days)
-              .enter().append("text")
-                .text(function (d) { return d; })
-                .attr("x", 0)
-                .attr("y", function (d, i) { return i * gridSize; })
-                .style("text-anchor", "end")
-                .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
-                .attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); });
-
-          var timeLabels = svg.selectAll(".timeLabel")
-              .data(times)
-              .enter().append("text")
-                .text(function(d) { return d; })
-                .attr("x", function(d, i) { return i * gridSize; })
-                .attr("y", 0)
-                .style("text-anchor", "middle")
-                .attr("transform", "translate(" + gridSize / 2 + ", -6)")
-                .attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis"); });
-
-          var heatMap = svg.selectAll(".hour")
-              .data(data)
-              .enter().append("rect")
-              .attr("x", function(d) { return (d.hour - 1) * gridSize; })
-              .attr("y", function(d) { return (d.well_id - 1) * gridSize; })
-              .attr("rx", 4)
-              .attr("ry", 4)
-              .attr("class", "hour bordered")
-              .attr("width", gridSize)
-              .attr("height", gridSize)
-              .style("fill", colors[0]);
-
-          heatMap.transition().duration(1000)
-              .style("fill", function(d) { return colorScale(d.value); });
-
-          heatMap.append("title").text(function(d) { return d.value; });
-          
-          var legend = svg.selectAll(".legend")
-              .data([0].concat(colorScale.quantiles()), function(d) { return d; })
-              .enter().append("g")
-              .attr("class", "legend");
-
-          legend.append("rect")
-            .attr("x", function(d, i) { return legendElementWidth * i; })
-            .attr("y", height)
-            .attr("width", legendElementWidth)
-            .attr("height", gridSize / 2)
-            .style("fill", function(d, i) { return colors[i]; });
-
-          legend.append("text")
-            .attr("class", "mono")
-            .text(function(d) { return "≥ " + Math.round(d); })
-            .attr("x", function(d, i) { return legendElementWidth * i; })
-            .attr("y", height + gridSize);
-      });
-}
