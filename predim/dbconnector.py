80d446c05e120c91830bb14f685da1d13b149978
Ramanujam Srivatsan <sramanujam@pivotal.io>
2015-06-17 19:15:12 -0700

    plugging in line plot for drill-down on cell + adding connection pooling with psycopg2

---
+++ predim/dbconnector.py
@@ -0,0 +1,95 @@
+"""
+   Wrapper around psycopg2's connection pool class. Pandas will call this 
+   class to obtain a connection and then work with it to query the database
+   Srivatsan Ramanujam <sramanujam@pivotal.io>, June 2015
+"""
+
+import psycopg2
+from psycopg2 import pool
+import pandas.io.sql as psql
+import os
+import json
+import ConfigParser
+
+class DBConnect(object):
+    def __init__(self, logger, conn_str=None):
+        """Constructor"""
+        self.logger = logger
+        host, port, user, database, password, app_port = None, None, None, None, None, None
+        if(not conn_str):
+            if(os.getenv("VCAP_APP_PORT")):
+                app_port = int(os.getenv("VCAP_APP_PORT"))
+                vcap_services = json.loads(os.environ['VCAP_SERVICES'])
+                creds = vcap_services['user-provided'][0]['credentials']
+                host = creds['host']
+                user = creds['user']
+                database = creds['database']
+                password = creds['password']
+                port = creds['port']
+            else:
+                #default port
+                app_port = 9090
+                #Read database credentials from user supplied file
+                basepath = os.path.dirname(__file__)
+                conf = ConfigParser.ConfigParser()
+                conf.read(os.path.join(basepath,'user.cred'))
+                logger.debug('Config sections:'+','.join(conf.sections()))
+                #host, port, user, database, password
+                host = conf.get('database_creds','host')
+                port = conf.get('database_creds','port')
+                user = conf.get('database_creds','user')
+                database = conf.get('database_creds','database')
+                password = conf.get('database_creds','password')
+
+                #Initialize connection string
+                conn_str =  """dbname='{database}' user='{user}' host='{host}' port='{port}' password='{password}'""".format(                       
+                                database=database,
+                                host=host,
+                                port=port,
+                                user=user,
+                                password=password
+                            )
+        self.conn_str = conn_str
+        self.__initConnectionPool__()
+
+    def __initConnectionPool__(self):
+        """
+           Initialize a connection pool
+        """
+        self.pool = psycopg2.pool.SimpleConnectionPool(1, 10, self.conn_str)
+
+    def __is_dbconn_alive__(self):
+        """ 
+           Ping the DB and check if the connection is alive - it seems like conn.closed doesn't work all the time 
+        """
+        ping_cmd = """select 1;"""
+        conn_alive = True
+        isQuery = True
+        try:
+            df = psql.read_sql(ping_cmd, self.getConnection())
+        except psycopg2.Error, e:
+            logger.error('Database connection is not alive: '+e)
+            conn_alive = False
+        return conn_alive
+
+    def __reconnect_if_closed__(self, conn_str=None):
+        """
+           Reconnect if the connection has failed/timed-out or has been closed down
+        """
+        if(self.pool.closed or not self.__is_dbconn_alive__()):
+            self.pool.closeall()
+            logger.warn('Detected closed connections. Reconnecting...')
+            self.__initConnectionPool__()
+
+    def getConnection(self):
+        """
+           Fetch a connection from the connection pool
+        """ 
+        return self.pool.getconn()
+
+    def fetchDataFrame(self, query):
+        """
+           Execute query
+        """
+        self.__reconnect_if_closed__()
+        return psql.read_sql(query, self.getConnection())
